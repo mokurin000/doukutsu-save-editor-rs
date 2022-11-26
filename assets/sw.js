066b57b358e4086cfc2022c6478f8d6a1254197f
poly000 <1348292515@qq.com>
2022-11-26 14:31:20 +0800

    fix broken link to web res

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './doukutsu-save-editor.js',
+  './doukutsu-save-editor_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
