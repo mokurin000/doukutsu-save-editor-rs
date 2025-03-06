a10ac0606d4f8ef731ddb98a0731dff0075153f2
mokurin000 <1348292515a@gmail.com>
2025-03-06 15:50:55 +0800

    chore: introduce template main

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,60 @@
 use egui::Vec2;
 
 fn main() {
+    // Log to stdout (if you run with `RUST_LOG=debug`).
+    tracing_subscriber::fmt::init();
+
+    main_inner();
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main_inner() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+            )
+            .await;
+
+        // Remove the loading text and spinner:
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main_inner() {
     use tokio::runtime::Runtime;
     use tokio::time;
 
@@ -15,9 +69,6 @@ fn main() {
         hardware_acceleration: eframe::HardwareAcceleration::Preferred,
         ..Default::default()
     };
-
-    // Log to stdout (if you run with `RUST_LOG=debug`).
-    tracing_subscriber::fmt::init();
 
     let async_rt = Runtime::new().unwrap();
     let _guard = async_rt.enter();
