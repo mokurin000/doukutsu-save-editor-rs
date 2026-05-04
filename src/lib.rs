366ec9d887186a9a915fa903fbe2ebf40ef34a00
莯凛 <mokurin000@gmail.com>
2026-05-04 16:43:13 +0800

    chore: cfg unused imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
 mod app;
+
+#[cfg(not(target_arch = "wasm32"))]
 use std::{future::Future, pin::Pin};
 
 pub use app::MainApp;
@@ -9,4 +11,5 @@ pub use app::MainApp;
 pub static TASK_SENDER: std::sync::OnceLock<kanal::Sender<BoxedFuture<()>>> =
     std::sync::OnceLock::new();
 
+#[cfg(not(target_arch = "wasm32"))]
 type BoxedFuture<T> = Pin<Box<dyn Send + Future<Output = T>>>;
